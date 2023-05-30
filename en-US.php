c55058de35c17211601cb36d0a2e2636c7c3feac
Herotrap <53874535+Herotrap@users.noreply.github.com>
2023-05-31 00:12:33 +0200

    Comment

--- en-US.php
+++ en-US.php
@@ -2,7 +2,7 @@
 /* General stuff */
 $LANGS['language'] = 'English (US)';
 $LANGS['languageenglish'] = 'English (US)';
-$LANGS['languagecode'] = 'en_US.UTF-8';
+$LANGS['languagecode'] = 'en_US.UTF-8'; /* Leave the ".UTF-8" in the end as-is, as it might break some dates */
 $LANGS['languagechange'] = 'Language has been set to English (US).';
 $LANGS['numberformat'] = 1; /* If your language uses comma separators (example: 1,000,000), value will be 1. If it uses dots (example: 1.000.000) or doesn't use separators, value will be 0.*/
 
